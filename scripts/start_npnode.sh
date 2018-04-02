4bdeb6d67cbf650c6c40818f6d218b7ba826798d
zhaohh <966986@qq.com>
2018-04-02 16:41:35 +0800

    没有删除干净

--- scripts/start_npnode.sh
+++ scripts/start_npnode.sh
@@ -1,6 +1,3 @@
-cd testnet_np
-rm -rf blocks blockchain std*
-cd ..
 ../programs/eosd/eosd --data-dir testnet_np > testnet_np/stdout.txt 2> testnet_np/stderr.txt &
 echo Launched eosd.
 echo See testnet_np/stderr.txt for eosd output.
